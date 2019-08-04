82883d116bac427aaafa4ca4c5abea808f2131e5
Gábor Kovács <gabor950601@gmail.com>
2019-08-04 10:56:54 +0200

    removed banner dimensions for test

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,8 +13,7 @@ class Banner extends React.Component {
                 <Carousel wrap={true}>
                     <Carousel.Item>
                         <img
-                            height="500"
-                            width="500"
+                            
                             className="d-block w-100"
                             src="https://images.pexels.com/photos/1537008/pexels-photo-1537008.jpeg?cs=srgb&dl=architect-architecture-black-and-white-1537008.jpg&fm=jpg"
                             alt="First slide"
@@ -22,8 +21,7 @@ class Banner extends React.Component {
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
-                            height="500"
-                            width="500"
+                            
                             className="d-block w-100"
                             src="https://images.pexels.com/photos/2219024/pexels-photo-2219024.jpeg?cs=srgb&dl=action-cement-construction-2219024.jpg&fm=jpg"
                             alt="Third slide"
@@ -31,8 +29,7 @@ class Banner extends React.Component {
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
-                            height="500"
-                            width="500"
+                            
                             className="d-block w-100"
                             src="https://images.pexels.com/photos/159045/the-interior-of-the-repair-interior-design-159045.jpeg?cs=srgb&dl=arrangement-color-colors-159045.jpg&fm=jpg"
                             alt="Third slide"
@@ -41,8 +38,7 @@ class Banner extends React.Component {
 
                     <Carousel.Item>
                         <img
-                            height="500"
-                            width="500"
+                            
                             className="d-block w-100"
                             src="https://images.pexels.com/photos/194096/pexels-photo-194096.jpeg?cs=srgb&dl=architecture-background-brick-194096.jpg&fm=jpgg"
                             alt="Third slide"
