6cdcee5cf850dc18a07809cf2053d456312296c2
Gábor Kovács <gabor950601@gmail.com>
2019-08-04 15:54:49 +0200

    added fade in to menu

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,23 +5,25 @@ class NavigationBar extends React.Component {
 
     render() {
         return (
-            <Navbar sticky="top">
-                <Container>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                            Menü
+            <div data-aos="fade-in">
+                <Navbar sticky="top">
+                    <Container>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                                Menü
   </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Küldetésünk</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Munkáink</Dropdown.Item>
-                            <Dropdown.Item className="request-class" href="#/action-3">Ajánlat kérés</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Csapatunk</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Kapcsolat</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Container>
-            </Navbar>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Küldetésünk</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Munkáink</Dropdown.Item>
+                                <Dropdown.Item className="request-class" href="#/action-3">Ajánlat kérés</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Csapatunk</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Kapcsolat</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Container>
+                </Navbar>
+            </div>
         );
     }
 }
