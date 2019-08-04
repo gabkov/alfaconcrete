8a634b47029e22b286e2962c694f97d04c9a3bed
Gábor Kovács <gabor950601@gmail.com>
2019-08-04 15:51:35 +0200

    added menu

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Dropdown } from 'react-bootstrap';
 
 class NavigationBar extends React.Component {
 
     render() {
         return (
-            <Navbar sticky="top" expand="lg" variant="light" bg="light">
+            <Navbar sticky="top">
                 <Container>
-                    <Navbar.Brand href="#">Küldetésünk</Navbar.Brand> 
-                    <Navbar.Brand href="#">Munkáink</Navbar.Brand>
-                    <Navbar.Brand href="#">Ajánlat kérés</Navbar.Brand>
-                    <Navbar.Brand href="#">Csapatunk</Navbar.Brand>
-                    <Navbar.Brand href="#">Kapcsolat</Navbar.Brand>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                            Menü
+  </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Küldetésünk</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Munkáink</Dropdown.Item>
+                            <Dropdown.Item className="request-class" href="#/action-3">Ajánlat kérés</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Csapatunk</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Kapcsolat</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </Container>
             </Navbar>
-            );
+        );
     }
 }
 
